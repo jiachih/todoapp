61d0dc033d5d7ce8b1aecc2a8c55fff2f9b50f87
jiachih <you@example.com>
2015-04-18 10:18:51 +0800

    change file

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,18 @@
 Rails.application.routes.draw do
+  resources :todos
+  member do
+   post 'done'
+  end
+end
+
+  
+
+  match ':controller(/:action(/:id(.:format)))', :via => :all
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
   # You can have the root of your site routed with "root"
-  # root 'welcome#index'
+  root 'todos#index'
 
   # Example of regular route:
   #   get 'products/:id' => 'catalog#view'
